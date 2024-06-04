5a3d8620c09d3ddc0d351ba3f6cea1490ed39f7d
~~ <~~>
2024-06-04 19:02:57 -0300

    segundo commit

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 function converterCelsiusParaFahrenheit() {
     const celsius = parseFloat(document.getElementById('celsius').value);
-    const fahrenheit = (celsius * 9/5) + 32;
+    const fahrenheit = (celsius * 1.8) + 32;
     document.getElementById('fahrenheit').value = fahrenheit.toFixed(2);
-    document.getElementById('resultado').textContent = `${celsius}°C equivalem a ${fahrenheit.toFixed(2)}°F`;
+    document.getElementById('resultado').textContent = `${celsius}°C equivalem a ${fahrenheit.toFixed(2)}°F`;   
 }
 
 function converterFahrenheitParaCelsius() {
     const fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
-    const celsius = (fahrenheit - 32) * 5/9;
+    const celsius = (fahrenheit - 32) / 1.8;
     document.getElementById('celsius').value = celsius.toFixed(2);
     document.getElementById('resultado').textContent = `${fahrenheit.toFixed(2)}°F equivalem a ${celsius.toFixed(2)}°C`;
 }
